c444d17444cb1cc5e0f80ce467460e850f859726
Jon Schlinkert <github@sellside.com>
2013-02-22 01:03:54 -0500

    corrected Gruntfile typo, made formatting more consistent, made better use of templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,11 @@
 /*
  * Assemble
- * http://sellside.github.com/assemelbe
+ * http://github.com/assemble/assemble
  *
  * Copyright (c) 2013 Assemble
  * MIT License
  */
+
 
 module.exports = function(grunt) {
 
@@ -42,15 +43,8 @@ module.exports = function(grunt) {
         options: {
           release: 'patch'
         },
-        src: ['package.json']
+        src: ['<%= version.check.src %>']
       }
-    },
-
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      files: ['Gruntfile.js', 'lib/**/*.js', 'tasks/**/*.js', 'test/**/*.js']
     },
 
     // Run tests.
@@ -63,25 +57,49 @@ module.exports = function(grunt) {
       }
     },
 
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      files: [
+        'Gruntfile.js',
+        'lib/**/*.js',
+        'tasks/**/*.js',
+        'test/**/*.js'
+      ]
+    },
+
     concat: {
       dist: {
-        src: ['<banner:meta.banner>', '<file_strip_banner:lib/<%= pkg.name %>.js>'],
+        src: [
+          '<banner:meta.banner>',
+          '<file_strip_banner:lib/<%= pkg.name %>.js>'
+        ],
         dest: 'dist/<%= pkg.name %>.js'
       }
     },
 
     min: {
       dist: {
-        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/<%= pkg.name %>.min.js'
+        src: [
+          '<banner:meta.banner>',
+          '<config:concat.dist.dest>'
+        ],
+        dest: '<%= pkg.name %>.min.js'
       }
     },
 
     uglify: {},
 
+
+    // Run other gruntfiles in project
+    subgrunt: {
+      examples: ['examples/**/Gruntfile.js']
+    },
+
     watch: {
       all: {
-        files: ['Gruntfile.js', 'lib/**/*.js', 'tasks/**/*.js', 'test/**/*.js'],
+        files: ['<%= jshint.files %>'],
         tasks: ['jshint', 'mochaTest'],
         options: {
           debounceDelay: 250
@@ -91,21 +109,28 @@ module.exports = function(grunt) {
 
   });
 
-  //  grunt.loadTasks('./tasks');
+  // Load npm plugins to provide necessary tasks.
+  // "grunt-version": "https://github.com/kswedberg/grunt-version/tarball/master"
+  // issue with putting this in the package.json file is that it updates it's own line since it has version": in it.
+  grunt.loadNpmTasks('grunt-version');
   grunt.loadNpmTasks('grunt-release');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-test');
 
-  //"grunt-version": "https://github.com/kswedberg/grunt-version/tarball/master"
-  // issue with putting this in the package.json file is that it updates it's own line since it has version": in it.
-  grunt.loadNpmTasks('grunt-version');
+  // Load local tasks.
+  grunt.loadTasks('tasks');
 
   // Default task.
   grunt.registerTask('default', [
     'jshint',
     'mochaTest',
     'watch'
+  ]);
+
+  // Default task.
+  grunt.registerTask('examples', [
+    'subgrunt'
   ]);
 
   // Tests to be run.
