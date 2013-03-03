3ca1e62e4b91155cc79bd1d882d8e8dd89d20087
Jon Schlinkert <github@sellside.com>
2013-03-03 11:12:31 -0500

    removed outdated comment

--- tasks/assemble.js
+++ tasks/assemble.js
@@ -268,7 +268,7 @@ module.exports = function(grunt) {
           }
 
           file.write(destFile, result);
-          grunt.log.ok('File ' + (filename + options.ext).magenta + ' created.' + ' ok '.green); // ✔ doesn't work for some reason
+          grunt.log.ok('File ' + (filename + options.ext).magenta + ' created.' + ' ok '.green);
         }); // build
 
       }); // filePair.src.forEach
