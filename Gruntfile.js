b6c47ca0dd4155880efec8454a6057f01d0eca0d
Jon Schlinkert <github@sellside.com>
2013-03-12 15:52:19 -0400

    moving some devDependencies into dependencies and removing unnecessary tasks from Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,6 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
     meta: {
       banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
@@ -20,21 +19,6 @@ module.exports = function(grunt) {
         '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
         ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
     },
-
-    // Release management
-    release: {
-      options: {
-        pkgBump: true,
-        compBump: true,
-        add: false,
-        commit: false,
-        tag: false,
-        push: false,
-        pushTags: false,
-        npm: false
-      }
-    },
-
     jshint: {
       files: [
         'Gruntfile.js',
@@ -46,7 +30,6 @@ module.exports = function(grunt) {
         jshintrc: '.jshintrc'
       }
     },
-
     // Run tests.
     mochaTest: {
       files: ['test/**/*.js']
@@ -55,45 +38,11 @@ module.exports = function(grunt) {
       options: {
         reporter: 'nyan'
       }
-    },
-
-
-    concat: {
-      dist: {
-        src: [
-          '<banner:meta.banner>',
-          '<file_strip_banner:lib/<%= pkg.name %>.js>'
-        ],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-
-    min: {
-      dist: {
-        src: [
-          '<banner:meta.banner>',
-          '<config:concat.dist.dest>'
-        ],
-        dest: '<%= pkg.name %>.min.js'
-      }
-    },
-
-    uglify: {},
-
-    watch: {
-      all: {
-        options: { debounceDelay: 250 },
-        files: ['<%= jshint.files %>'],
-        tasks: ['jshint', 'mochaTest']
-      }
     }
-
   });
 
   // Load npm plugins to provide necessary tasks.
-  grunt.loadNpmTasks('grunt-release');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-test');
 
   // Load local tasks.
@@ -104,14 +53,7 @@ module.exports = function(grunt) {
     'test'
   ]);
 
-  // Default task.
-  grunt.registerTask('bump', [
-    'test',
-    'release'
-  ]);
-
   // Tests to be run.
-  // Placeholder for Travis CI until proper tests are setup.
   grunt.registerTask('test', [
     'jshint',
     'mochaTest'
