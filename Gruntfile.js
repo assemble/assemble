06b340a21e126d9a2f536218f79cbe3ef386f745
Jon Schlinkert <dev@sellside.com>
2013-05-25 22:06:59 -0400

    add assets tests/examples to test folder and Grunfile. Run assemble

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,11 +75,39 @@ module.exports = function(grunt) {
           'test/actual/multi/dest2/': ['test/files/**/*.{md,markdown}'],
           'test/actual/multi/dest2/sub-dest/': ['test/files/**/*.hbs', '!test/files/layout*.*']
         }
+      },
+      assets_one: {
+        options: {
+          assets: 'test/actual/public',
+          assets_one: true
+        },
+        files: {'test/actual/assets-public-folder.html': ['test/files/assets.hbs']}
+      },
+      assets_two: {
+        options: {
+          assets: 'test/actual',
+          assets_two: true
+        },
+        files: {'test/actual/assets-same-folder.html': ['test/files/assets.hbs']}
+      },
+      assets_three: {
+        options: {
+          assets: '.',
+          assets_three: true
+        },
+        files: {'test/actual/assets-root.html': ['test/files/assets.hbs']}
       }
+    },
+
+    // Before assembling new files, removed previously
+    // created files.
+    clean: {
+      tests: ['test/actual/**/*.html']
     }
   });
 
   // Load npm plugins to provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-mocha-test');
 
@@ -87,8 +115,8 @@ module.exports = function(grunt) {
   grunt.loadTasks('tasks');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'assemble']);
+  grunt.registerTask('default', ['jshint', 'clean', 'assemble']);
 
   // Tests to be run.
-  grunt.registerTask('test', ['jshint', 'mochaTest']);
+  grunt.registerTask('test', ['default', 'mochaTest']);
 };
