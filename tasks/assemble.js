f402a30d8e0529ae474b384135ee85c7ebe8650c
Mikko Tapionlinna <mikko@tapionlinna.fi>
2013-06-25 21:56:14 +0300

    Changing collections to be objects instead of arrays
    This is related to what was discussed in assemble/assemble#216

--- tasks/assemble.js
+++ tasks/assemble.js
@@ -206,7 +206,7 @@ module.exports = function(grunt) {
       var pages = [];
       var collections = {};
       assemble.options.collections.forEach(function(item) {
-        collections[item] = [];
+        collections[item] = {};
       });
 
       var assetsPath = assemble.options.assets;
