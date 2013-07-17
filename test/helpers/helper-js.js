2912d53b7be76efb5fef7030dddbcc155596e19d
Jon Schlinkert <dev@sellside.com>
2013-07-17 00:23:52 -0400

    adds missing helpers to fix the "X property cannot be found" issue resulting from refactoring the handlebars-helpers project.

---
+++ test/helpers/helper-js.js
@@ -0,0 +1,37 @@
+(function() {
+  module.exports.register = function(Handlebars, options) {
+
+    var path = require('path');
+
+    var getExt = function(str) {
+      var extname = path.extname(str);
+      if (extname) {
+        str = extname;
+      }
+      if (str[0] === ".") {
+        str = str.substring(1);
+      }
+      return str;
+    };
+    Handlebars.registerHelper("js", function(context) {
+      if (!Array.isArray(context)) {
+        context = [context];
+      }
+      return new Handlebars.SafeString(context.map(function(item) {
+        var ext = getExt(item);
+        var js = '<script src="' + options.assets + '/js/' + item + '"></script>';
+        var coffee = '<script type="text/coffeescript" src="' + options.assets + '/js/' + item + '"></script>';
+        switch (ext) {
+          case "js":
+            return js;
+          case "coffee":
+            return coffee;
+          default:
+            return js;
+        }
+      }).join("\n"));
+    });
+
+
+  };
+}).call(this);
