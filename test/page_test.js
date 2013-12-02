30d326fcf5ad5395a2dc103b1b5a688787cedea3
Brian Woodward <brian.woodward@gmail.com>
2013-12-02 16:23:12 -0500

    refactor(utils/models): adding utils for models and some models
    Adding some utility functions for dealing with models and model
    inheritance.
    
    Adding a Component and Page model to start off with.
    
    Adding tests around the Page model

---
+++ test/page_test.js
@@ -0,0 +1,39 @@
+/**
+ * Assemble
+ *
+ * Assemble <http://assemble.io>
+ * Created and maintained by Jon Schlinkert and Brian Woodward
+ *
+ * Copyright (c) 2013 Upstage.
+ * Licensed under the MIT License (MIT).
+ */
+
+var expect = require('chai').expect;
+var inspect = require('util').inspect;
+
+var assemble = require('../lib/assemble');
+
+describe('page', function() {
+  describe('model', function() {
+
+    it('should create a new page model', function() {
+      var actual = new assemble.models.Page();
+      expect(actual).to.be.an.instanceof(assemble.models.Page);
+    });
+
+    it('should have default properties', function() {
+      var actual = new assemble.models.Page();
+      expect(actual).to.have.property('src');
+    });
+
+    it('should initialize given options', function() {
+      var options = {
+        foo: 'bar'
+      };
+      var actual = new assemble.models.Page(options);
+      console.log(inspect(actual));
+      expect(actual).to.have.property('foo');
+    });
+
+  });
+});
