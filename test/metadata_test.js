48561e8d8b4566e2587facf53da84c1c05d3a1bb
Brian Woodward <brian.woodward@gmail.com>
2013-12-02 18:50:11 -0500

    refactor(metadata): creating metadata store
    Creating a metadata store that will store information about
    assemble between runs. This will also be used to only run through
    files when they change.

---
+++ test/metadata_test.js
@@ -0,0 +1,37 @@
+/**
+ * Assemble
+ *
+ * Assemble <http://assemble.io>
+ * Created and maintained by Jon Schlinkert and Brian Woodward
+ *
+ * Copyright (c) 2013 Upstage.
+ * Licensed under the MIT License (MIT).
+ */
+
+var expect = require('chai').expect;
+
+var inspect = require('util').inspect;
+
+var assemble = require('../lib/assemble');
+
+describe('metadata', function() {
+
+  var metadata = null;
+  var contents = 'These are my contents';
+
+  before(function() {
+    metadata = new assemble.models.Metadata();
+  });
+  
+  it('should store a file', function(done) {
+    metadata.setFile('test', contents, done);
+  });
+
+  it('should retreive a file', function(done) {
+    metadata.getFile('test', function(err, actual) {
+      expect(actual.hash).to.equal(contents);
+      done();
+    });
+  });
+
+});
