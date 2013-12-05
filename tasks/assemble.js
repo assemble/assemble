32324d3d0172edadab900d5b3218dd4be2abe7e4
Jon Schlinkert <dev@sellside.com>
2013-12-05 08:12:55 -0500

    adds grunt.config.process to pageContext so that lodash templates are processed. previously, lodash templates were only processed for the context of the "current page" but were left un-processed for the "other pages".

--- tasks/assemble.js
+++ tasks/assemble.js
@@ -299,6 +299,7 @@ module.exports = function(grunt) {
 
                 var pageInfo = yfm.extract(page, {fromFile: false});
                 pageContext = useFileInfo ? (fileInfo.data || fileInfo.metadata || {}) : pageInfo.context;
+                pageContext = grunt.config.process(pageContext);
 
                 // Page object
                 var pageObj = {
