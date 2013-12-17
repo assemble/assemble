f7a0fa0d89c2dcacd63445dde3b2cbdabfbe69f4
Brian Woodward <brian.woodward@gmail.com>
2013-12-17 09:20:09 -0500

    changing the main class Assemble to Line since that's the class that does the work

--- test/assemble_creation_test.js
+++ test/assemble_creation_test.js
@@ -15,30 +15,30 @@ var assemble = require('../lib/assemble');
 
 describe('assemble', function() {
 
-  describe('Assemble instances', function() {
+  describe('Line instances', function() {
   
-    it('should create an instance of Assemble', function() {
+    it('should create an instance of Line', function() {
       var actual = assemble();
-      expect(actual).to.be.an.instanceof(assemble.Assemble);
+      expect(actual).to.be.an.instanceof(assemble.Line);
     });
 
-    it('should create an instance of Assemble and store it in the instanceCache', function() {
+    it('should create an instance of Line and store it in the instanceCache', function() {
       var actual = assemble();
       expect(assemble.instanceCache).to.have.property('default');
     });
 
-    it('should create a named instance of Assemble and store it in the instanceCache', function() {
+    it('should create a named instance of Line and store it in the instanceCache', function() {
         var actual = assemble('test');
         expect(assemble.instanceCache).to.have.property('test');
     });
 
-    it('should create an instance of Assemble and be able to retrieve the same instance from instanceCache', function() {
+    it('should create an instance of Line and be able to retrieve the same instance from instanceCache', function() {
       var expected = assemble('test2');
       var actual = assemble('test2');
       expect(expected).to.eql(actual);
     });
 
-    it('should create two instances of Assemble that are different', function() {
+    it('should create two instances of Line that are different', function() {
       var expected = assemble('test3');
       var actual = assemble('test4');
       expect(expected).to.not.eql(actual);
