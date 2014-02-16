d1b9e645c018555229ab6a6a66d10490523e2ec7
Brian Woodward <brian.woodward@gmail.com>
2014-02-16 18:47:33 -0500

    getting assemble to work with a source and metadata passed in

---
+++ test/source_test.js
@@ -0,0 +1,48 @@
+  /**
+ * Assemble
+ *
+ * Assemble <http://assemble.io>
+ * Created and maintained by Jon Schlinkert and Brian Woodward
+ *
+ * Copyright (c) 2013 Upstage.
+ * Licensed under the MIT License (MIT).
+ */
+
+var expect = require('chai').expect;
+var _ = require('lodash');
+
+var assemble = require('../lib/assemble');
+
+describe('source', function() {
+
+  it('should render from a string', function(done) {
+    var source = "Render string.";
+    assemble(source, {name: 'source-test-1'}).build(function (err, results) {
+      if (err) {
+        console.log('Error:', err);
+      }
+      expect(_.keys(results.components).length).to.eql(1);
+      expect(results.components[_.keys(results.components)[0]].content).to.eql(source);
+      done();
+    });
+  });
+
+  it('should render from a string using context', function(done) {
+    var source = "Render string {{foo}}.";
+    var expected = "Render string with context.";
+    assemble(source, {
+      name: 'source-test-2',
+      metadata: {
+        foo: 'with context'
+      }
+    }).build(function (err, results) {
+      if (err) {
+        console.log('Error:', err);
+      }
+      expect(_.keys(results.components).length).to.eql(1);
+      expect(results.components[_.keys(results.components)[0]].content).to.eql(expected);
+      done();
+    });
+  });
+
+});
