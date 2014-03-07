9c8054419af9581412c3c3b8116a66dc132c43fd
Brian Woodward <brian.woodward@gmail.com>
2014-03-07 12:52:18 -0500

    adding plugin loader test to make sure plugins get loaded

--- examples/plugins/example-coffee.coffee
+++ examples/plugins/example-coffee.coffee
@@ -1,9 +1,9 @@
 ###
- Assemble <http://assemble.io>
+Assemble <http://assemble.io>
 
- Copyright (c) 2014 Jon Schlinkert, Brian Woodward, contributors
- Licensed under the MIT License (MIT).
- ###
+Copyright (c) 2014 Jon Schlinkert, Brian Woodward, contributors
+Licensed under the MIT License (MIT).
+###
 
 'use strict'
 
---
+++ test/plugin_test.js
@@ -0,0 +1,36 @@
+  /**
+ * Assemble
+ *
+ * Assemble <http://assemble.io>
+ * Created and maintained by Jon Schlinkert and Brian Woodward
+ *
+ * Copyright (c) 2013 Upstage.
+ * Licensed under the MIT License (MIT).
+ */
+
+var expect = require('chai').expect;
+var path = require('path');
+var _ = require('lodash');
+
+var assemble = require('../lib/assemble');
+
+describe('plugin', function() {
+
+  it('should load plugins', function(done) {
+    var options = {
+      name: 'plugin-test-1',
+      source: 'Some Template',
+      metadata: {
+        plugins: [path.join(__dirname, '../examples/plugins/example-js.js')]
+      }
+    };
+    assemble(options).build(function (err, results) {
+      if (err) {
+        console.log('Error:', err);
+      }
+      expect(_.keys(results.plugins).length).to.eql(2);
+      done();
+    });
+  });
+
+});
