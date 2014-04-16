7b10515961fc7d1a83779b526d484e1a762d5a57
Brian Woodward <brian.woodward@gmail.com>
2014-04-16 12:41:34 -0400

    adding collection processing, collection index page processing, and tests for both

--- test/plugin_collections_test.js
+++ test/plugin_collections_test.js
@@ -43,4 +43,82 @@ describe('plugins collections', function() {
 
   });
 
+  it('should create a collection index page for a collection', function (done) {
+
+    var componentOpts = {
+      src: 'test-component',
+      name: 'test-component',
+      metadata: {
+        title: 'This is a test component',
+        tags: [
+          'first',
+          'second',
+          'third',
+          'fourth',
+          'fifth',
+          'sixth',
+          'seventh',
+          'eight',
+          'ninth',
+          'tenth'
+        ]
+      },
+      raw: '{{title}}',
+      content: '{{title}}'
+    };
+
+    var components = [];
+    for (var i = 1; i <= 20; i++) {
+      var component = new assemble.models.Component(componentOpts);
+      component.src += ' ' + i;
+      component.name += ' ' + i;
+      component.metadata.title += ' ' + i;
+      components.push(component);
+    }
+
+    var assembleOpts = {
+      name: name(),
+      metadata: {
+        components: components,
+        collections: [
+          {
+            name: 'tag',
+            plural: 'tags',
+            // Index of all tags
+            index: {
+              template: 'test/fixtures/templates/collections/tags/index.hbs',
+              pagination: {
+                prop: ':num',
+                limit: 3,
+                sortby: '',
+                sortOrder: 'ASC'
+              },
+              permalinks: {
+                structure: 'tags/:num/index.html'
+              }
+            }
+          }
+        ]
+      }
+    };
+    assemble(assembleOpts).build(function(err, results) {
+      if (err) {
+        console.log('Error', err);
+        return done(err);
+      }
+      //console.log('components', results.components);
+      expect(results.components).to.have.property('collections-tags-1');
+      expect(results.components).to.have.property('collections-tags-2');
+      expect(results.components).to.have.property('collections-tags-3');
+      expect(results.components).to.have.property('collections-tags-4');
+
+      expect(results.components['collections-tags-1'].metadata.tags.length).to.eql(3);
+      expect(results.components['collections-tags-2'].metadata.tags.length).to.eql(3);
+      expect(results.components['collections-tags-3'].metadata.tags.length).to.eql(3);
+      expect(results.components['collections-tags-4'].metadata.tags.length).to.eql(1);
+
+      done();
+    });
+  });
+
 });
