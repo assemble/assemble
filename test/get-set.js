65c64ee883294edcc64fbfc9c51b745c32acec9a
jonschlinkert <dev@sellside.com>
2014-07-15 03:31:20 -0400

    more get/set tests

--- test/get-set.js
+++ test/get-set.js
@@ -12,14 +12,25 @@ var should = require('should');
 var assemble = require('..');
 
 describe('assemble config', function () {
-  beforeEach(function() {
-    assemble.clearCache();
+  afterEach(function() {
+    assemble.clear();
   });
 
   describe('.set()', function () {
-    it("should set properties on the `cache` object.", function () {
+    it('should set a value', function () {
       assemble.set('a', 'b');
       assemble.get('a').should.equal('b');
+    });
+
+    it("should set properties on the `cache` object.", function () {
+      assemble.set('a', 'b');
+      assemble.cache.a.should.equal('b');
+    });
+
+    it("should allow an object to be set directly.", function () {
+      assemble.set({x: 'y'});
+      assemble.cache.x.should.equal('y');
+      assemble.get('x').should.equal('y');
     });
 
     it("should set nested properties on the `cache` object.", function () {
@@ -27,13 +38,53 @@ describe('assemble config', function () {
       assemble.get('c').d.should.equal('e');
     });
 
-    it("should use dot notation to `get` values.", function () {
+    it("should use dot notation to `set` values.", function () {
       assemble.set('h.i', 'j');
+      assemble.get('h').should.eql({i: 'j'});
+    });
+
+    it("should use dot notation to `get` values.", function () {
+      assemble.set('h', {i: 'j'});
       assemble.get('h.i').should.equal('j');
     });
 
-    it('should set a value', function () {
+    it('should return `this` for chaining', function () {
       assemble.set('a', 'b').should.equal(assemble);
+      assemble
+        .set('aa', 'bb')
+        .set('bb', 'cc')
+        .set('cc', 'dd');
+      assemble.get('aa').should.equal('bb');
+      assemble.get('bb').should.equal('cc');
+      assemble.get('cc').should.equal('dd');
+    });
+
+    it('should expand template strings in the config.', function () {
+      assemble.set('j', {k: '${l}', l: 'm'}, true);
+      assemble.cache.j.k.should.eql('m');
+      assemble.get('j.k').should.eql('m');
+    });
+
+    it('should expand template strings in the config.', function () {
+      assemble
+        .set('l', 'm')
+        .set('j', {k: '${l}'}, true);
+      assemble.cache.j.k.should.eql('m');
+      assemble.get('j.k').should.eql('m');
+    });
+
+    it("should expand template strings when an object is set.", function () {
+    	assemble.set({a: 'b'});
+      assemble.set({c: '<%= a %>'});
+      assemble.cache.c.should.equal('b');
+      assemble.get('c').should.equal('b');
+    });
+
+    xit('should not expand values that are set after the template is defined.', function () {
+      assemble
+        .set('j', {k: '${l}'}, true)
+        .set('l', 'm');
+      assert(assemble.get('j.k') === undefined);
     });
 
     it('should return the assemble when undefined', function () {
