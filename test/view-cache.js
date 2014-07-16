0e96301df332e4b1d2814bf90bf48ac0c6fa5564
Brian Woodward <brian.woodward@gmail.com>
2014-07-16 15:00:52 -0400

    refactor layouts

---
+++ test/view-cache.js
@@ -0,0 +1,22 @@
+/**
+ * Assemble <http://assemble.io>
+ *
+ * Copyright (c) 2014, Jon Schlinkert, Brian Woodward, contributors.
+ * Licensed under the MIT License (MIT).
+ */
+
+'use strict';
+
+var assert = require('assert');
+var should = require('should');
+var assemble = require('..');
+
+
+describe('assemble view-cache', function () {
+  xdescribe('.lookup()', function () {
+
+    it('should return filepath when absolute', function () {
+    });
+
+  });
+});
