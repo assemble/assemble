ba27d839b1f8f928a08c0dcc5e27b9408c4c62e1
Brian Woodward <brian.woodward@gmail.com>
2014-07-17 18:55:57 -0400

    making layout engines work with delimiter-map

---
+++ test/engines.js
@@ -0,0 +1,41 @@
+/**
+ * Assemble <http://assemble.io>
+ *
+ * Copyright (c) 2014, Jon Schlinkert, Brian Woodward, contributors.
+ * Licensed under the MIT License (MIT).
+ */
+
+'use strict';
+
+var assert = require('assert');
+var should = require('should');
+var assemble = require('..');
+var Layout = require('../lib/view/layout');
+
+
+describe('assemble engines', function () {
+  describe('.engine()', function () {
+
+    beforeEach(function () {
+      assemble.init();
+    });
+
+    it('should set an engine with the given extension', function () {
+      var handlebars = require('../lib/engine/handlebars');
+      assemble.engine('hbs', handlebars);
+      should.exist(assemble.engines['.hbs']);
+      should.exist(assemble.engines['.hbs'].renderFile);
+      should.exist(assemble.engines['.hbs'].render);
+    });
+
+    it('should set a layout engine when setting an engine', function () {
+      var handlebars = require('../lib/engine/handlebars');
+      assemble.engine('hbs', handlebars);
+
+      var layoutEngine = new Layout(assemble);
+      should.exist(layoutEngine._layouts['.hbs']);
+      should.exist(layoutEngine._layouts['.hbs']._bodyRe);
+    });
+
+  });
+});
