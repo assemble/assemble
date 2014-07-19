dbeb9e58f6d09b1ffa785f07560125a42f137df9
jonschlinkert <dev@sellside.com>
2014-07-19 07:15:28 -0400

    Layouts class

---
+++ test/layouts.js
@@ -0,0 +1,20 @@
+/**
+ * Assemble <http://assemble.io>
+ *
+ * Copyright (c) 2014, Jon Schlinkert, Brian Woodward, contributors.
+ * Licensed under the MIT License (MIT).
+ */
+
+'use strict';
+
+var should = require('should');
+var assemble = require('..');
+
+
+describe('assemble layouts', function () {
+  describe('.layouts()', function () {
+    xit('should cache layouts.', function () {
+      //
+    });
+  });
+});
