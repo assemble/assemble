43815aee43dd6da85ce4bee531f3d9096c79508c
jonschlinkert <dev@sellside.com>
2014-07-20 13:49:48 -0400

    gulpfile jshint

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,12 @@ var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
 
 
-gulp.task('mocha', ['js'], function () {
+gulp.task('mocha', ['jshint'], function () {
   gulp.src('test/*.js')
     .pipe(mocha({reporter: 'spec'}));
 });
 
-gulp.task('js', function() {
+gulp.task('jshint', function() {
   gulp.src(['*.js', 'lib/*.js', 'test/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
