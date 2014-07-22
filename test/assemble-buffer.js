501e5157053159160d59f27107d98c09c8f06d17
Brian Woodward <brian.woodward@gmail.com>
2014-07-22 11:09:04 -0400

    assemble.collection and assemble._buffer tests

---
+++ test/assemble-buffer.js
@@ -0,0 +1,34 @@
+/**
+ * Assemble <http://assemble.io>
+ *
+ * Copyright (c) 2014, Jon Schlinkert, Brian Woodward, contributors.
+ * Licensed under the MIT License (MIT).
+ */
+
+'use strict';
+
+var assert = require('assert');
+var should = require('should');
+var assemble = require('..');
+
+describe('assemble buffer', function () {
+
+  beforeEach(function () {
+      assemble.init();
+  });
+
+  describe('._buffer()', function () {
+
+    it('should return a stream', function (done) {
+      var stream = assemble._buffer();
+      should.exist(stream);
+      should.exist(stream.on);
+      done();
+    });
+
+    xit('should buffer files', function () {
+    });
+
+  });
+
+});
