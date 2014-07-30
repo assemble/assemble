998ba5c5d83d8e89ee0121c44da2d888241dc0ec
jonschlinkert <dev@sellside.com>
2014-07-30 11:58:29 -0400

    scaffold out some tests

---
+++ test/assemble-run.js
@@ -0,0 +1,20 @@
+/**
+ * Assemble <http://assemble.io>
+ *
+ * Copyright (c) 2014, Jon Schlinkert, Brian Woodward, contributors.
+ * Licensed under the MIT License (MIT).
+ */
+
+'use strict';
+
+var should = require('should');
+var assemble = require('..');
+
+
+describe('assemble run', function () {
+  describe('assemble.run()', function () {
+    xit('should run a task', function () {
+      // TODO
+    });
+  });
+});
---
+++ test/assemble-use.js
@@ -0,0 +1,20 @@
+/**
+ * Assemble <http://assemble.io>
+ *
+ * Copyright (c) 2014, Jon Schlinkert, Brian Woodward, contributors.
+ * Licensed under the MIT License (MIT).
+ */
+
+'use strict';
+
+var should = require('should');
+var assemble = require('..');
+
+
+describe('assemble use', function () {
+  describe('assemble.use()', function () {
+    xit('should use a plugin', function () {
+      // TODO
+    });
+  });
+});
