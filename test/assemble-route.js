0977923f6dccc4d6fa94c122d3945393985d779d
Brian Woodward <brian.woodward@gmail.com>
2014-07-30 18:37:16 -0400

    routes tests

--- test/assemble-route.js
+++ test/assemble-route.js
@@ -5,18 +5,17 @@ var should = require('should');
 var join = require('path').join;
 var rimraf = require('rimraf');
 var fs = require('graceful-fs');
+var path = require('path');
 require('mocha');
 
 var outpath = join(__dirname, './out-fixtures');
 
 
 describe('assemble route', function() {
-  before (function () {
-    assemble.init();
-  });
 
   describe('route()', function() {
     beforeEach(function (done) {
+      assemble.init();
       rimraf(outpath, done);
     });
     afterEach(function (done) {
@@ -29,7 +28,7 @@ describe('assemble route', function() {
         next();
       });
 
-      var instream = assemble.src(join(__dirname, './fixtures/**/*.txt'));
+      var instream = assemble.src(join(__dirname, './fixtures/routes/*.txt'));
       var outstream = assemble.dest(outpath);
       instream.pipe(outstream);
 
@@ -39,8 +38,101 @@ describe('assemble route', function() {
         should.exist(file);
         should.exist(file.path);
         should.exist(file.contents);
-        join(file.path, '').should.equal(join(outpath, './copy/example.txt'));
+        join(file.path, '').should.equal(join(outpath, 'example.txt'));
         String(file.contents).should.equal('THIS IS A TEST');
+      });
+
+      outstream.on('end', function () {
+        done();
+      });
+    });
+
+
+    it('should set multiple routes on different files', function (done) {
+
+      assemble.route(/\.txt/, function (file, next) {
+        file.contents = new Buffer(file.contents.toString().toUpperCase());
+        next();
+      });
+
+      assemble.route(/\.hbs/, function (file, next) {
+        file.path = join(path.dirname(file.path), path.basename(file.path, path.extname(file.path)) + '.html');
+        next();
+      });
+
+      var instream = assemble.src(join(__dirname, './fixtures/routes/*.*'));
+      var outstream = assemble.dest(outpath);
+      instream.pipe(outstream);
+
+      outstream.on('error', done);
+      outstream.on('data', function (file) {
+        // data should be re-emitted correctly
+        should.exist(file);
+        should.exist(file.path);
+        should.exist(file.contents);
+        // join(file.path, '').should.equal(join(outpath, 'example.html'));
+        // String(file.contents).should.equal('THIS IS A TEST');
+      });
+
+      outstream.on('end', function () {
+        done();
+      });
+    });
+
+    it('should set multiple routes on same file', function (done) {
+
+      assemble.route(/\.txt/, function (file, next) {
+        file.contents = new Buffer(file.contents.toString().toUpperCase());
+        next();
+      });
+
+      assemble.route(/example/, function (file, next) {
+        file.ext = '.html';
+        next();
+      });
+
+      var instream = assemble.src(join(__dirname, './fixtures/routes/*.txt'));
+      var outstream = assemble.dest(outpath);
+      instream.pipe(outstream);
+
+      outstream.on('error', done);
+      outstream.on('data', function (file) {
+        // data should be re-emitted correctly
+        should.exist(file);
+        should.exist(file.path);
+        should.exist(file.contents);
+        join(file.path, '').should.equal(join(outpath, 'example.html'));
+        String(file.contents).should.equal('THIS IS A TEST');
+      });
+
+      outstream.on('end', function () {
+        done();
+      });
+    });
+
+    it('should handle errors', function (done) {
+
+      assemble.route(/\.hbs/, function (file, next) {
+        throw new Error('This is an error');
+        next();
+      }, function (err, file, next) {
+        should.exist(err);
+        err.message.should.equal('This is an error');
+        next();
+      });
+
+      var instream = assemble.src(join(__dirname, './fixtures/routes/*.*'));
+      var outstream = assemble.dest(outpath);
+      instream.pipe(outstream);
+
+      outstream.on('error', done);
+      outstream.on('data', function (file) {
+        // data should be re-emitted correctly
+        should.exist(file);
+        should.exist(file.path);
+        should.exist(file.contents);
+        // join(file.path, '').should.equal(join(outpath, 'example.txt'));
+        // String(file.contents).should.equal('THIS IS A TEST');
       });
 
       outstream.on('end', function () {
@@ -56,7 +148,7 @@ describe('assemble route', function() {
         next();
       });
 
-      var instream = assemble.src(join(__dirname, './fixtures/**/*.txt'));
+      var instream = assemble.src(join(__dirname, './fixtures/routes/*.txt'));
       var outstream = assemble.dest(outpath);
 
       instream
@@ -69,7 +161,7 @@ describe('assemble route', function() {
         should.exist(file);
         should.exist(file.path);
         should.exist(file.contents);
-        join(file.path, '').should.equal(join(outpath, './copy/example.txt'));
+        join(file.path, '').should.equal(join(outpath, 'example.txt'));
         String(file.contents).should.equal('foo: THIS IS A TEST');
       });
 
