525267e727d68d22cfefa0ca9a490c7608f3fa91
Brian Woodward <brian.woodward@gmail.com>
2014-07-31 12:26:16 -0400

    Minor fix for loader and updating Page tests

--- test/Page.js
+++ test/Page.js
@@ -9,6 +9,9 @@
 
 var assert = require('assert');
 var should = require('should');
+var path = require('path');
+var debug = require('debug')('test:Page');
+
 var Loader = require('../lib/loader.js');
 var assemble = require('..');
 
@@ -21,6 +24,7 @@ describe('Page', function () {
   describe('new Loader()', function () {
     it('should create a new instance of Loader', function () {
       var page = new Loader();
+      debug('page', page);
       should.exist(page);
       page.should.be.instanceOf(Loader);
     });
@@ -29,17 +33,148 @@ describe('Page', function () {
   describe('assemble.page()', function () {
     it('should create a new instance of Page', function () {
       var page = assemble.page();
+      debug('page', page);
       should.exist(page);
       page.should.be.instanceOf(Loader);
     });
   });
 
   describe('.normalize()', function () {
-    it('should normalize files to a new vinyl file object.', function () {
-      var page = new Loader();
-      var pages = page.normalize('test/fixtures/templates/no-helpers/*.hbs');
 
-      console.log(pages)
+    describe('string', function () {
+      it('should normalize files to a new vinyl file object.', function () {
+        var page = new Loader();
+        var pages = page.normalize('test/fixtures/templates/no-helpers/*.hbs');
+        debug('pages', pages);
+        pages.length.should.equal(4);
+      });
     });
+
+    describe('object with one file', function () {
+      it('should normalize files to a new vinyl file object.', function () {
+        var page = new Loader();
+        var config = {
+          filepath: 'foo.hbs',
+          data: {
+            name: 'Brian'
+          },
+          content: '---\ntitle: Foo\n---\nHi {{name}} from {{title}}\n'
+        };
+        debug('config', config);
+        var pages = page.normalize(config);
+        debug('pages', pages);
+        pages.length.should.equal(1);
+        pages[0].path.should.equal(path.join(process.cwd(), 'foo.hbs'));
+        pages[0].contents.toString().should.equal('\nHi {{name}} from {{title}}\n');
+        pages[0].data.should.eql({title: 'Foo', name: 'Brian'});
+      });
+    });
+
+    describe('object with many files', function () {
+      it('should normalize files to a new vinyl file object.', function () {
+        var page = new Loader();
+        var config = {
+          'foo.hbs': {
+            filepath: 'foo.hbs',
+            data: {
+              name: 'Brian'
+            },
+            content: '---\ntitle: Foo\n---\nHi {{name}} from {{title}}\n'
+          },
+          'bar.hbs': {
+            filepath: 'bar.hbs',
+            data: {
+              name: 'Jon'
+            },
+            content: '---\ntitle: Bar\n---\nHi {{name}} from {{title}}\n'
+          }
+        };
+        debug('config', config);
+        var pages = page.normalize(config);
+        debug('pages', pages);
+        pages.length.should.equal(2);
+        pages[0].path.should.equal(path.join(process.cwd(), 'foo.hbs'));
+        pages[0].contents.toString().should.equal('\nHi {{name}} from {{title}}\n');
+        pages[0].data.should.eql({title: 'Foo', name: 'Brian'});
+
+        pages[1].path.should.equal(path.join(process.cwd(), 'bar.hbs'));
+        pages[1].contents.toString().should.equal('\nHi {{name}} from {{title}}\n');
+        pages[1].data.should.eql({title: 'Bar', name: 'Jon'});
+
+      });
+
+    });
+
+    describe('array', function () {
+      it('should normalize files to a new vinyl file object.', function () {
+        var page = new Loader();
+        var config = [
+          {
+            filepath: 'foo.hbs',
+            data: {
+              name: 'Brian'
+            },
+            content: '---\ntitle: Foo\n---\nHi {{name}} from {{title}}\n'
+          },
+          {
+            filepath: 'bar.hbs',
+            data: {
+              name: 'Jon'
+            },
+            content: '---\ntitle: Bar\n---\nHi {{name}} from {{title}}\n'
+          }
+        ];
+        debug('config', config);
+        var pages = page.normalize(config);
+        debug('pages', pages);
+        pages.length.should.equal(2);
+        pages[0].path.should.equal(path.join(process.cwd(), 'foo.hbs'));
+        pages[0].contents.toString().should.equal('\nHi {{name}} from {{title}}\n');
+        pages[0].data.should.eql({title: 'Foo', name: 'Brian'});
+
+        pages[1].path.should.equal(path.join(process.cwd(), 'bar.hbs'));
+        pages[1].contents.toString().should.equal('\nHi {{name}} from {{title}}\n');
+        pages[1].data.should.eql({title: 'Bar', name: 'Jon'});
+
+      });
+    });
+
+    describe('function', function () {
+      it('should normalize files to a new vinyl file object.', function () {
+        var page = new Loader();
+        var config = function () {
+          return [
+            {
+              filepath: 'foo.hbs',
+              data: {
+                name: 'Brian'
+              },
+              content: '---\ntitle: Foo\n---\nHi {{name}} from {{title}}\n'
+            },
+            {
+              filepath: 'bar.hbs',
+              data: {
+                name: 'Jon'
+              },
+              content: '---\ntitle: Bar\n---\nHi {{name}} from {{title}}\n'
+            }
+          ];
+        };
+        debug('config', config);
+        var pages = page.normalize(config);
+        debug('pages', pages);
+        pages.length.should.equal(2);
+        pages[0].path.should.equal(path.join(process.cwd(), 'foo.hbs'));
+        pages[0].contents.toString().should.equal('\nHi {{name}} from {{title}}\n');
+        pages[0].data.should.eql({title: 'Foo', name: 'Brian'});
+
+        pages[1].path.should.equal(path.join(process.cwd(), 'bar.hbs'));
+        pages[1].contents.toString().should.equal('\nHi {{name}} from {{title}}\n');
+        pages[1].data.should.eql({title: 'Bar', name: 'Jon'});
+
+      });
+    });
+
+
   });
 });
