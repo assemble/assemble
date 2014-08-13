fa964c665a5f7c75646fd90faee96afae7528894
Brian Woodward <brian.woodward@gmail.com>
2014-08-13 17:29:14 -0400

    Tests for templates
    Test for template types with and without layouts.

--- test/assemble.template.js
+++ test/assemble.template.js
@@ -31,5 +31,41 @@ describe('assemble init', function () {
       should.exist(assemble.lions);
       should.exist(assemble.cache.lions);
     });
+
+    it('should add load new templates add store them on the cache for the custom template type', function () {
+      assemble.template('doowb', {plural: 'doowbs'});
+      assemble.doowb({
+        name: 'brian',
+        data: {
+          first: 'Brian'
+        },
+        content: '---\nlast: Woodward\n---\nHi this is {{first}} {{last}}'
+      });
+
+      should.exist(assemble.cache.doowbs.brian);
+      assemble.cache.doowbs.brian.contents.toString().should.equal('Hi this is {{first}} {{last}}');
+      assemble.cache.doowbs.brian.original.toString().should.equal('---\nlast: Woodward\n---\nHi this is {{first}} {{last}}');
+      should.exist(assemble.cache.doowbs.brian.data.first);
+      should.exist(assemble.cache.doowbs.brian.data.last);
+    });
+
+    it('should add load new templates add store them on the cache for the custom template type as layouts', function () {
+      assemble.template('jon', {plural: 'jons', isLayout: true});
+      assemble.jon({
+        name: 'jon',
+        data: {
+          first: 'Jon',
+          ext: '.hbs'
+        },
+        content: '---\nlast: Schlinkert\n---\nHi this is {{first}} {{last}}'
+      });
+
+      should.exist(assemble.cache.jons.jon);
+      assemble.cache.jons.jon.contents.toString().should.equal('Hi this is {{first}} {{last}}');
+      assemble.cache.jons.jon.original.toString().should.equal('---\nlast: Schlinkert\n---\nHi this is {{first}} {{last}}');
+      should.exist(assemble.cache.jons.jon.data.first);
+      should.exist(assemble.cache.jons.jon.data.last);
+      should.exist(assemble.layoutSettings['.hbs'].cache.jon);
+    });
   });
 });
