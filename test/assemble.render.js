680a05440f83ae3b91e3388e3e21dd8b114fb56b
Brian Woodward <brian.woodward@gmail.com>
2014-08-14 11:39:16 -0400

    Using context-manager for partials
    Using the context-manager for getting partial data.

--- test/assemble.render.js
+++ test/assemble.render.js
@@ -7,14 +7,24 @@
 
 'use strict';
 
+var path = require('path');
 var should = require('should');
-var assemble = require('..');
+var Assemble = require('..');
 
+var actual = __dirname + '/render-actual';
 
 describe('assemble render', function () {
   describe('assemble.render()', function () {
-    xit('should render a file', function () {
-      // TODO
+    var assemble = null;
+    beforeEach(function () {
+      assemble = Assemble.create();
+    });
+
+    it('should render a file', function () {
+      assemble.partials('test/fixtures/includes/*.hbs');
+      assemble.layouts('test/fixtures/layouts/*.hbs');
+      assemble.src('test/fixtures/pages/*.hbs')
+        .pipe(assemble.dest(actual));
     });
   });
 });
