b687a708d45434590a0863474eb78c863eb5cc11
Brian Woodward <brian.woodward@gmail.com>
2014-09-12 11:49:50 -0400

    using listen and expanding options when set
    When options are set on the config-cache, process them and add them to the data object.

---
+++ test/assemble.listen.js
@@ -0,0 +1,37 @@
+/**
+ * assemble <https://github.com/assemble/assemble>
+ *
+ * Copyright (c) 2014, Jon Schlinkert, Brian Woodward, contributors.
+ * Licensed under the MIT License (MIT).
+ */
+
+'use strict';
+
+var should = require('should');
+var assemble = require('..');
+
+describe('assemble listen', function() {
+  var site = null;
+  beforeEach(function () {
+    site = assemble.create();
+  });
+
+  describe('.listen()', function() {
+    it('should expand and process options when added', function() {
+      site.data({ foo: 'bar' });
+      site.option({ baz: '<%= foo %>' });
+      site.get('data').foo.should.equal('bar');
+      site.get('data').baz.should.equal('bar');
+      site.option('baz').should.equal('<%= foo %>');
+    });
+
+    it('should expand and process options.data when added', function() {
+      site.data({ foo: 'bar' });
+      site.option({ data: { baz: '<%= foo %>' } });
+      site.get('data').foo.should.equal('bar');
+      site.get('data').baz.should.equal('bar');
+      site.option('data').should.eql({ baz: '<%= foo %>' });
+    });
+  });
+
+});
