fa10aa85f55f23a7f9540ccf468e71150b397eda
Brian Woodward <brian.woodward@gmail.com>
2014-10-21 23:17:03 -0400

    init and renderer
    init and renderer plugins using `.page`

--- b.js
+++ b.js
@@ -1,9 +1,30 @@
 
 var assemble = require('./');
+assemble.engine('hbs', require('engine-handlebars'));
 var tap = require('gulp-tap');
 
-assemble.src('./test/fixtures/templates/no-helpers/*.hbs')
-  .pipe(tap(function (file) {
-    console.log('file', file);
-  }))
-  .pipe(assemble.dest('_test_b'));
+var src = assemble.src('./test/fixtures/templates/no-helpers/*.hbs');
+src.on('error', function (err) {
+  console.log('src error', err);
+  console.log();
+});
+
+var dest = assemble.dest('_test_b');
+dest.on('error', function (err) {
+  console.log('dest', err);
+  console.log();
+});
+
+var peek = tap(function (file) {
+  // console.log('tap', file.contents.toString());
+});
+
+src.pipe(peek).pipe(dest).on('error', function (err) {
+  console.log('pipeline error', err);
+  console.log();
+});
+
+
+// setTimeout(function () {
+//   console.log(assemble.cache.pages);
+// }, 1000);
