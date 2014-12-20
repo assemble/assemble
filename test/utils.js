787de959ba5489e8544218187920e1aa19327bf4
jonschlinkert <jon.schlinkert@sellside.com>
2014-12-20 12:48:42 -0500

    cleanup tests

--- test/utils.js
+++ test/utils.js
@@ -7,31 +7,10 @@
 
 'use strict';
 
-var matter = require('gray-matter');
 var should = require('should');
 var utils = require('../lib/utils');
 
 describe('utils', function() {
-  describe('.noop()', function () {
-    it('should return the same value passed in', function () {
-      utils.noop('foo').should.eql('foo');
-    });
-  });
-
-  describe('.noopStream()', function () {
-    it('should return a passthrough stream', function () {
-      var stream = utils.noopStream();
-      should.exist(stream);
-      should.exist(stream.on);
-    });
-  });
-
-  describe('.fixPath()', function () {
-    it('should fix a path with a lowercase drive letter', function () {
-      utils.fixPath('c:\\', 'some', 'file', 'path')[0].should.equal('C');
-    });
-  });
-
   describe('.formatExt()', function () {
     it('should prepend a . to the extension', function () {
       utils.formatExt('hbs').should.equal('.hbs');
@@ -47,17 +26,6 @@ describe('utils', function() {
     });
     it('should keep the extension', function () {
       utils.stripExtDot('hbs').should.equal('hbs');
-    });
-  });
-
-  describe('.engineDelims()', function () {
-    it('should return the correct delimiters when extension contains a dot', function () {
-      utils.engineDelims('.hbs').should.eql(['{{', '}}']);
-      utils.engineDelims('.handlebars').should.eql(['{{', '}}']);
-    });
-    it('should return the correct delimiters when extension does not contain a dot', function () {
-      utils.engineDelims('hbs').should.eql(['{{', '}}']);
-      utils.engineDelims('handlebars').should.eql(['{{', '}}']);
     });
   });
 });
