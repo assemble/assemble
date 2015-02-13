84d80f70b0aee64e9a6e464315c526d1b8afcb0e
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-13 08:10:34 -0500

    adds noop engine, banner

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
  * assemble <https://github.com/assemble/assemble>
  *
- * Copyright (c) 2014 Jon Schlinkert, Brian Woodward, contributors.
+ * Copyright (c) 2014-2015 Jon Schlinkert, Brian Woodward.
  * Licensed under the MIT license.
  */
 
