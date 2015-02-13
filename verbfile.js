e927ded27dd733c7a6e7a1325131611f1e5878fc
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-13 08:57:44 -0500

    adds istanbul task

--- verbfile.js
+++ verbfile.js
@@ -1,26 +1,34 @@
-var verb = require('verb');
+var istanbul = require('gulp-istanbul');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
-
-verb.task('mocha', function () {
-  verb.src('test/*.js')
-    .pipe(mocha({reporter: 'spec'}));
-});
-
-verb.task('jshint', function() {
-  verb.src(['*.js', 'lib/*.js', 'test/*.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
-});
+var verb = require('verb');
 
 verb.task('readme', function () {
   verb.src('.verb.md')
     .pipe(verb.dest('./'));
 });
 
-verb.task('docs', function () {
+verb.task('apidocs', function () {
   verb.src('docs/_verb/*.md')
     .pipe(verb.dest('docs'));
 });
 
-verb.task('default', ['mocha', 'jshint', 'readme', 'docs']);
+verb.task('lint', function() {
+  verb.src(['index.js', 'lib/**/*.js', 'test/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+verb.task('test', function (cb) {
+  verb.src(['index.js', 'lib/**/*.js', 'bin/*.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function () {
+      verb.src('test/*.js')
+        .pipe(mocha())
+        .pipe(istanbul.writeReports())
+        .on('end', cb);
+    });
+});
+
+verb.task('default', ['test', 'lint', 'readme', 'apidocs']);
