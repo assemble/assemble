5568280c15fd59cd351d3c3d18d8c53665aaa66d
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-29 19:16:41 -0400

    legacy grunt warning

--- tasks/assemble.js
+++ tasks/assemble.js
@@ -1,8 +1,5 @@
 /*!
- * assemble <https://github.com/assemble/assemble>
- *
- * Copyright (c) 2014-2015 Jon Schlinkert, Brian Woodward.
- * Licensed under the MIT license.
+ * Heads up
  */
 
 'use strict';
@@ -10,51 +7,23 @@
 module.exports = function(grunt) {
   grunt.registerMultiTask('assemble', 'Compile template files with specified engines', function() {
     var done = this.async();
+    var chalk = require('chalk');
+    var wrap = require('word-wrap');
+    console.log();
+
     var msg = [
-      'WARNING!!!',
+      chalk.red('HEADS UP!!! Assemble is now a standalone node.js library. To continue using assemble as a grunt plugin, please do the following:'),
       '',
-      'Use `grunt-assemble` with `grunt` instead of `assemble`.',
+      chalk.gray('    npm i grunt-assemble -save-dev'),
       '',
-      'Install:',
-      '  `npm i grunt-assemble -save-dev`',
-      '  `grunt.loadNpmTask(\'grunt-assemble\');`',
+      chalk.red('Then use the the following in your project\'s Gruntfile.js:'),
+      '',
+      chalk.gray('    grunt.loadNpmTask(\'grunt-assemble\');'),
       ''
     ].join('\n');
 
-    console.log(format(msg, 80, '*'));
+    console.log(wrap(msg, 55));
+    console.log();
     return done(false);
-
-    function format (msg, len, border) {
-      var repeat = require('repeat-string');
-      var longest = require('longest');
-      var wrap = require('word-wrap');
-      var chalk = require('chalk');
-
-      // wrap the text down to the length size
-      var txt = wrap(msg, {
-        width: len - 4,
-        indent: ' ',
-        newline: ' \n '
-      });
-
-      // find the length of the longest line
-      var lines = txt.split('\n');
-      var max = longest(lines).length;
-      max = (max > len ? max : len);
-
-      // make the top and bottom borders
-      var end = repeat(border, max + 2);
-
-      // prepare the loop
-      var res = end + '\n', i = 0;
-      len = lines.length;
-      while (len--) {
-        // use the max line length to pad each line and show borders
-        var line = lines[i++];
-        var llen = line.length;
-        res += border + line + repeat(' ', max - llen) + border + '\n';
-      }
-      return chalk.red(res + end);
-    }
   });
 };
