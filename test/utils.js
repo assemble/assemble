a10d929c1fd23656215562cc16cf33ee08f4cad2
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-29 15:30:47 -0400

    update utils/tests

--- test/utils.js
+++ test/utils.js
@@ -11,12 +11,14 @@ var should = require('should');
 var utils = require('../lib/utils');
 
 describe('utils', function() {
-  describe('.formatExt()', function () {
-    it('should prepend a . to the extension', function () {
-      utils.formatExt('hbs').should.equal('.hbs');
+  describe('.basename()', function () {
+    it('should return the basename of a file without extension:', function () {
+      utils.basename('foo.hbs').should.equal('foo');
+      utils.basename('foo.md').should.equal('foo');
     });
-    it('should keep the extension', function () {
-      utils.formatExt('.hbs').should.equal('.hbs');
+
+    it('should remove the extname passed as the second argument:', function () {
+      utils.basename('foo.bar.baz', '.bar.baz').should.equal('foo');
     });
   });
 });
