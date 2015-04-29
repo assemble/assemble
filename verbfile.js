f51b44ed05c8d8869162372dfdec7d658dab14fe
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-29 15:31:17 -0400

    generate summary report

--- verbfile.js
+++ verbfile.js
@@ -37,13 +37,16 @@ verb.task('lint', function() {
 });
 
 verb.task('test', function (cb) {
-  verb.src(['index.js', 'lib/**/*.js', 'bin/*.js'])
+  verb.src(['index.js', 'lib/**/*.js'])
     .pipe(istanbul())
     .pipe(istanbul.hookRequire())
     .on('finish', function () {
       verb.src('test/*.js')
         .pipe(mocha())
-        .pipe(istanbul.writeReports())
+        .pipe(istanbul.writeReports({
+          reporters: [ 'text' ],
+          reportOpts: {dir: 'coverage', file: 'summary.txt'}
+        }))
         .on('end', cb);
     });
 });
