aaa29645bc99b37bf77506f3562ce9e7a7a395b3
jonschlinkert <jon.schlinkert@sellside.com>
2015-05-03 21:58:40 -0400

    rename `inst` to `app` in assemble.files.js

--- test/assemble.files.js
+++ test/assemble.files.js
@@ -10,78 +10,57 @@
 var assert = require('assert');
 var should = require('should');
 var assemble = require('..');
-var inst = null;
+var app;
 
 describe('assemble.files', function() {
   describe('outside a task', function() {
     beforeEach(function () {
-      inst = assemble.init();
+      app = assemble.init();
     });
 
     it('should get files from the `pages` collection', function(done) {
-      inst.pages('home', { path: 'home.hbs', content: 'This is home.' });
-      Object.keys(inst.views.pages).length.should.equal(1);
-      Object.keys(inst.files).length.should.equal(1);
-      inst.views.pages.should.have.property('home');
-      inst.files.should.have.property('home');
-      inst.files.should.eql(inst.views.pages);
+      app.pages('home', { path: 'home.hbs', content: 'This is home.' });
+      Object.keys(app.views.pages).length.should.equal(1);
+      Object.keys(app.files).length.should.equal(1);
+      app.views.pages.should.have.property('home');
+      app.files.should.have.property('home');
+      app.files.should.eql(app.views.pages);
       done();
     });
   });
 
   describe('inside tasks', function () {
-    beforeEach(function () {
-      inst = assemble.init();
-    });
-
-    it('should get files from the task specific collection', function (done) {
-      inst.task('test', function () {
-        var stream = inst.src('test/fixtures/templates/partials/*.hbs');
-        stream.on('end', function () {
-          var collection = inst.inflections[inst.getTask()];
-          assert(Object.keys(inst.views[collection]).length === 3);
-          assert(Object.keys(inst.files).length === 3);
-          inst.views[collection].should.have.properties(['a', 'b', 'c']);
-          inst.files.should.have.properties(['a', 'b', 'c']);
-          inst.files.should.equal(inst.views[collection]);
-        });
-        return stream;
-      });
-      inst.task('default', ['test'], function () { done(); });
-      inst.run('default');
-    });
-
     it('should get files for the correct task', function (done) {
-      inst.task('test-a', function () {
-        var stream = inst.src('test/fixtures/templates/partials/*.hbs');
+      app.task('test-a', function () {
+        var stream = app.src('test/fixtures/templates/partials/*.hbs');
         stream.on('end', function () {
-          Object.keys(inst.views['task_test-as']).length.should.eql(3);
-          Object.keys(inst.files).length.should.eql(3);
-          inst.views['task_test-as'].should.have.properties(['a', 'b', 'c']);
-          inst.files.should.have.properties(['a', 'b', 'c']);
-          inst.files.should.eql(inst.views['task_test-as']);
-          inst.views['task_test-as'].should.not.eql(inst.views['task_test-bs']);
-          inst.files.should.not.eql(inst.views['task_test-bs']);
+          Object.keys(app.views['test-as']).length.should.equal(3);
+          Object.keys(app.files).length.should.equal(3);
+          app.views['test-as'].should.have.properties(['a', 'b', 'c']);
+          app.files.should.have.properties(['a', 'b', 'c']);
+          app.files.should.eql(app.views['test-as']);
+          app.views['test-as'].should.not.eql(app.views['test-bs']);
+          app.files.should.not.eql(app.views['test-bs']);
         });
         return stream;
       });
 
-      inst.task('test-b', function () {
-        var stream = inst.src('test/fixtures/templates/partials/*.hbs');
+      app.task('test-b', function () {
+        var stream = app.src('test/fixtures/templates/partials/*.hbs');
         stream.on('end', function () {
-          Object.keys(inst.views['task_test-bs']).length.should.eql(3);
-          Object.keys(inst.files).length.should.eql(3);
-          inst.views['task_test-bs'].should.have.properties(['a', 'b', 'c']);
-          inst.files.should.have.properties(['a', 'b', 'c']);
-          inst.files.should.eql(inst.views['task_test-bs']);
-          inst.views['task_test-as'].should.not.eql(inst.views['task_test-bs']);
-          inst.files.should.not.eql(inst.views['task_test-as']);
+          Object.keys(app.views['test-bs']).length.should.equal(3);
+          Object.keys(app.files).length.should.equal(3);
+          app.views['test-bs'].should.have.properties(['a', 'b', 'c']);
+          app.files.should.have.properties(['a', 'b', 'c']);
+          app.files.should.eql(app.views['test-bs']);
+          app.views['test-as'].should.not.eql(app.views['test-bs']);
+          app.files.should.not.eql(app.views['test-as']);
         });
         return stream;
       });
 
-      inst.task('default', ['test-a', 'test-b'], function () { done(); });
-      inst.run('default');
+      app.task('default', ['test-a', 'test-b'], function () { done(); });
+      app.run('default');
     });
   });
 });
