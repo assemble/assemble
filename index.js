7c7e3ade904a3c877c9fc035d8ca2f9655a104ab
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-11 00:02:21 -0400

    adds magic, wonder and delight

--- index.js
+++ index.js
@@ -5,10 +5,14 @@
  */
 
 var path = require('path');
+var util = require('util');
+var async = require('async');
+var runtimes = require('composer-runtimes');
 var Boilerplate = require('boilerplate');
 var Scaffold = require('scaffold');
 var Composer = require('composer');
 var Templates = require('templates');
+var proto = Composer.prototype;
 var utils = require('./lib/utils');
 
 /**
@@ -30,24 +34,63 @@ function Assemble(options) {
   }
   Templates.apply(this, arguments);
   Composer.apply(this, arguments);
+  this.set('Boilerplate', this.options.Boilerplate || Boilerplate);
+  this.set('Scaffold', this.options.Scaffold || Scaffold);
+  this.boilerplates = {};
+  this.scaffolds = {};
   this.init();
 }
+
+Templates.inherit(Assemble, Composer);
+
+/**
+ * `Assemble` prototype methods
+ */
 
 Templates.extend(Assemble, {
   constructor: Assemble,
 
   init: function() {
-    this.engine(['hbs', 'html', 'md'], require('engine-handlebars'));
+    runtimes(this);
+    this.defaultEngine();
+    this.defaultMiddleware();
+    this.defaultViewTypes();
+    this.loaded = true;
+  },
 
+  /**
+   * Default template engine and related extensions.
+   */
+
+  defaultEngine: function () {
+    this.engine(['hbs', 'html', 'md'], require('engine-handlebars'));
+  },
+
+  /**
+   * Dfault middleware
+   *  | Ensure user-defined layout is recognized
+   *  | Parse front-matter
+   */
+
+  defaultMiddleware: function () {
     this.preLayout(/\.(hbs|md|html)$/, function (view, next) {
       if (!view.layout) view.layout = view.locals.layout || view.data.layout;
       next();
     });
-
     this.onLoad(/\.(hbs|md|html)$/, function (view, next) {
       utils.matter.parse(view, next);
     });
+  },
 
+  /**
+   * Default `viewTypes`
+   *  | partials
+   *  | layouts
+   *  | pages
+   *  | files
+   */
+
+  defaultViewTypes: function () {
     this.create('partials', {
       viewType: 'partial',
       renameKey: function (fp) {
@@ -68,21 +111,97 @@ Templates.extend(Assemble, {
       }
     });
 
-    this.loaded = true;
+    this.create('files', {
+      renameKey: function (fp) {
+        return fp;
+      }
+    });
   },
+
+
+  /**
+   * Copy a file from `src` to `dest` and process any templates in
+   * `file.contents`.
+   *
+   * @param {Object} `file` [Vinyl][] file object.
+   * @param {Object} `options`
+   * @return {Stream} Returns a stream to continue processing if needed.
+   * @api public
+   */
+
+  process: function (file, options) {
+    options = options || {};
+    var pre = options.preprocess || options.pipeline || utils.identity;
+    var post = options.postprocess || utils.identity;
+    var stream = this.src(file.src);
+    stream = pre(stream, this)
+      .pipe(this.renderFile(file.data || {}));
+    return post(stream, this)
+      .pipe(this.dest(file.dest));
+  },
+
+  /**
+   * Generate all of the `src`/`dest` definitions in the given
+   * `config` object. Used for generating [boilerplates][] and
+   * [scaffolds][].
+   *
+   * @param {Object} `config` The configuration object to use.
+   * @param {Function} `cb` Callback function, exposes `err` on the callback.
+   * @return {Object} Returns the `Assemble` instance for chaining.
+   * @api public
+   */
 
   generate: function (config, cb) {
-    // generate(config, cb);
+    var opts = config.options || {};
+    opts.preprocess = config.preprocess;
+    opts.postprocess = config.postprocess;
+    async.each(config.files, function (file, next) {
+      utils.parallel(this.process(file, opts), next);
+    }.bind(this), cb);
     return this;
   },
 
+  /**
+   * Register a boilerplate.
+   *
+   * @param {String} `name` The name of the boilerplate to register
+   * @param {Object} `config` The configuration object to use.
+   * @return {Object} Returns the `Boilerplate` instance for chaining.
+   * @api public
+   */
+
   boilerplate: function (name, config) {
-    this.boilerplates[name] = new Boilerplate(config);
-    return this;
+    var Boilerplate = this.get('Boilerplate');
+    var boilerplate = !(config instanceof Boilerplate)
+      ? new Boilerplate(config)
+      : config;
+
+    var app = this;
+    boilerplate.generate = function (options, cb) {
+      var targets = Object.keys(boilerplate.targets);
+      async.each(targets, function (target, next) {
+        app.generate(target, options, next);
+      }, cb);
+    };
+
+    this.boilerplates[name] = boilerplate;
+    return boilerplate;
   },
 
   scaffold: function (name, config) {
-    this.scaffolds[name] = new Scaffold(config);
+    var Scaffold = this.get('Scaffold');
+    var scaffold = !(config instanceof Scaffold)
+      ? new Scaffold(config)
+      : config;
+
+    var app = this;
+    scaffold.generate = function (cb) {
+      var args = [].slice.call(arguments);
+      args.unshift(this);
+      return app.generate.apply(app, args);
+    };
+
+    this.scaffolds[name] = scaffold;
     return this;
   },
 
@@ -98,9 +217,14 @@ Templates.extend(Assemble, {
    * @api public
    */
 
-  src: function (glob, options) {
+  src: function () {
     if (!this.loaded) this.init();
     return utils.vfs.src.apply(utils.vfs, arguments);
+  },
+
+  symlink: function () {
+    if (!this.loaded) this.init();
+    return utils.vfs.symlink.apply(utils.vfs, arguments);
   },
 
   /**
@@ -115,20 +239,9 @@ Templates.extend(Assemble, {
    * @api public
    */
 
-  dest: function (/*dest, options*/) {
-    return utils.dest.apply(utils.dest, arguments);
-  },
-
-  task: function (name) {
-    var task = Composer.prototype.task;
-    if (!this.loaded) this.init();
-    var key = 'task_' + name;
-    this.create(key, {
-      renameKey: function (fp) {
-        return fp;
-      }
-    });
-    return task.apply(this, arguments);
+  dest: function () {
+    return utils.vfs.dest.apply(utils.vfs, arguments)
+      .on('data', function () {})
   },
 
   /**
@@ -141,14 +254,14 @@ Templates.extend(Assemble, {
    * ```
    *
    * @param {String|Array} `patterns` Glob patterns of files to copy.
-   * @param  {String|Function} `destDir` Desination directory.
+   * @param  {String|Function} `dest` Desination directory.
    * @return {Stream} Stream, to continue processing if necessary.
    * @api public
    */
 
-  copy: function(patterns, destDir, opts) {
-    return utils.vfs.src(patterns, opts)
-      .pipe(utils.vfs.dest(destDir, opts));
+  copy: function(patterns, dest, options) {
+    return utils.vfs.src(patterns, options)
+      .pipe(utils.vfs.dest(dest, options));
   },
 
   /**
@@ -175,11 +288,6 @@ Templates.extend(Assemble, {
     });
   },
 
-  taskName: function () {
-    var task = this.session.get('task');
-    return (task ? 'task_' + task.name : 'file');
-  },
-
   /**
    * Render a vinyl file.
    *
@@ -190,27 +298,50 @@ Templates.extend(Assemble, {
    */
 
   renderFile: function (locals) {
-    // TODO: update template to use pascalcase
-    var File = this[this.taskName()];
-    var self = this;
+    var name = this.taskName();
+    var app = this;
 
     return utils.through.obj(function (file, enc, cb) {
       if (typeof locals === 'function') {
         cb = locals;
         locals = {};
       }
-
-      self.render(new File(file), locals, function (err, res) {
+      app[name].addView(file.path, file);
+      var view = app[name].getView(file.path);
+      app.render(view, locals, function (err, res) {
         if (err) return cb(err);
         res.contents = new Buffer(res.content);
         file = new utils.Vinyl(res);
         cb(null, file);
       });
     });
+  },
+
+  task: function (name) {
+    if (!this.loaded) this.init();
+    this.create('task_' + name, {
+      renameKey: function (fp) {
+        return fp;
+      }
+    });
+    return proto.task.apply(this, arguments);
+  },
+
+  taskName: function () {
+    var task = this.session.get('task');
+    return task
+      ? ('task_' + task.name)
+      : 'file';
+  },
+
+  run: function (name) {
+    return proto.run.apply(this, arguments);
+  },
+
+  watch: function (name) {
+    return proto.watch.apply(this, arguments);
   }
 });
-
-utils.extend(Assemble.prototype, Composer.prototype);
 
 /**
  * Expose the `Assemble` constructor
