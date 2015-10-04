380f0b976c575a147a1e8b004518093387d708dc
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-04 15:43:31 -0400

    git clone task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
+'use strict';
+
 var gulp = require('gulp');
-var mocha = require('gulp-mocha');
+var argv = require('minimist')(process.argv.slice(2));
+var through = require('through2');
+var stylish = require('jshint-stylish');
 var istanbul = require('gulp-istanbul');
 var jshint = require('gulp-jshint');
-require('jshint-stylish');
+var mocha = require('gulp-mocha');
+var git = require('gulp-git');
+var del = require('rimraf');
 
 var lint = ['index.js', 'lib/*.js'];
+function url(repo) {
+  return 'https://github.com/' + repo;
+}
+var repo = url('jonschlinkert/templates');
+
 
 gulp.task('coverage', function () {
   return gulp.src(lint)
@@ -12,26 +23,36 @@ gulp.task('coverage', function () {
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('mocha', ['coverage'], function () {
-  return gulp.src('test/*.js')
+gulp.task('test', ['coverage'], function () {
+  return gulp.src(['spec/*.js'])
     .pipe(mocha({reporter: 'spec'}))
+    .pipe(istanbul.writeReports())
     .pipe(istanbul.writeReports({
       reporters: [ 'text' ],
       reportOpts: {dir: 'coverage', file: 'summary.txt'}
-    }))
+    }));
 });
 
-gulp.task('jshint', function () {
+gulp.task('lint', function () {
   return gulp.src(lint)
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter(stylish));
 });
 
-gulp.task('default', ['mocha', 'jshint'], function (cb) {
-  console.log('Finished "default"');
+gulp.task('clone', ['clean'], function(cb) {
+  git.clone(repo, {args: '_temp'}, function (err) {
+    if (err) return cb(err);
 
-  // force the process to end since `verb.watch`
-  // holds it open in the tests
-  process.exit();
-  cb();
+    gulp.src('_temp/test/**')
+      .pipe(gulp.dest('spec'))
+      .on('end', function() {
+        del('_temp', cb);
+      });
+  });
 });
+
+gulp.task('clean', function (cb) {
+  del(argv.del || 'spec', cb);
+});
+
+gulp.task('default', ['test', 'lint']);
