6b37537d569cf6fd3762507f59abf6c619be515e
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-10 14:58:47 -0400

    update examples

--- examples/views.js
+++ examples/views.js
@@ -3,5 +3,7 @@ var assemble = require('../');
 var Views = assemble.Views;
 var views = new Views();
 
-views.addView({path: 'foo', contents: 'bar'});
+views.addView({path: 'a', contents: 'aaa'});
+views.addView({path: 'b', contents: 'bbb'});
+views.addView({path: 'c', contents: 'ccc'});
 console.log(views)
