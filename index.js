2dbece724b4975a3c1b5025e891dd56396111f16
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-10 14:58:38 -0400

    slim down

--- index.js
+++ index.js
@@ -4,13 +4,17 @@
  * module dependencies
  */
 
-var path = require('path');
 var only = require('emitter-only');
-var Templates = require('templates');
+var reloadViews = require('assemble-reload-views');
 var render = require('assemble-render-file');
-var stream = require('assemble-stream');
-var plugin = require('./lib/plugins');
-var utils = require('./lib/utils');
+var Core = require('templates');
+
+/**
+ * Local dependencies
+ */
+
+var composer = require('./lib/composer');
+var init = require('./lib/init');
 
 /**
  * Create an `assemble` application. This is the main function exported
@@ -28,7 +32,7 @@ function Assemble(options) {
   if (!(this instanceof Assemble)) {
     return new Assemble(options);
   }
-  Templates.apply(this, arguments);
+  Core.apply(this, arguments);
   this.options = options || {};
   this.init(this);
 }
@@ -37,101 +41,35 @@ function Assemble(options) {
  * `Assemble` prototype methods
  */
 
-Templates.extend(Assemble, {
+Core.extend(Assemble, {
   constructor: Assemble,
 
   /**
    * Initialize Assemble defaults
    */
 
-  init: function(app) {
-    this.define('isAssemble', true);
-    this.use(plugin.vinyl());
-    this.use(stream());
+  init: function() {
+    this.isAssemble = true;
+
+    /**
+     * Load core plugins
+     */
+
+    this.use(require('assemble-fs'));
+    this.use(require('assemble-streams'));
+    this.use(composer());
     this.use(render());
-    this.use(plugin.composer());
-    this.use(plugin.collections());
-    this.use(plugin.reloadViews());
-    this.use(plugin.generate());
-    this.use(plugin.process());
+    this.use(init());
+
+    this.use(reloadViews());
 
     /**
-     * Default template engine and related extensions.
+     * Allow events to be registered only once, so
+     * that we can reinitialize the application and
+     * avoid re-registering the same emitters.
      */
 
-    this.option('extensions', ['hbs', 'html', 'md']);
-
-    /**
-     * Default template engine and related extensions.
-     */
-
-    this.engine(this.options.extensions, utils.engine);
-
-    /**
-     * Default middleware for parsing front matter
-     */
-
-    this.onLoad(/\.(hbs|md|html)$/, function (view, next) {
-      utils.matter.parse(view, next);
-    });
-  },
-
-  /**
-   * Allow events to be registered only once, so
-   * that we can reinitialize the application and
-   * avoid re-registering the same emitters.
-   */
-
-  only: function () {
-    return only.apply(this, arguments);
-  },
-
-  /**
-   * Copy files with the given glob `patterns` to the specified `dest`.
-   *
-   * ```js
-   * app.task('assets', function() {
-   *   app.copy('assets/**', 'dist/');
-   * });
-   * ```
-   * @name .copy
-   * @param {String|Array} `patterns` Glob patterns of files to copy.
-   * @param  {String|Function} `dest` Desination directory.
-   * @return {Stream} Stream, to continue processing if necessary.
-   * @api public
-   */
-
-  copy: function(patterns, dest, options) {
-    return utils.vfs.src(patterns, options)
-      .pipe(utils.vfs.dest(dest, options))
-      .on('data', function() {})
-  },
-
-  /**
-   * Push a view collection into a vinyl stream.
-   *
-   * ```js
-   * app.toStream('posts', function(file) {
-   *   return file.path !== 'index.hbs';
-   * })
-   * ```
-   * @name .toStream
-   * @param {String} `collection` The name of the view collection to push into the stream.
-   * @param {Function} Optionally pass a filter function to use for filtering views.
-   * @return {Stream}
-   * @api public
-   */
-
-  toStream: function (name) {
-    var views = this.getViews(name) || {};
-    var stream = utils.through.obj();
-    setImmediate(function () {
-      Object.keys(views).forEach(function (key) {
-        stream.write(views[key]);
-      });
-      stream.end();
-    });
-    return utils.srcStream(stream);
+    this.mixin('only', only.bind(this));
   }
 });
 
