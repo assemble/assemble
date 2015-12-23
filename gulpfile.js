0e5f49cc1b5bc33f3bc9a92fa689f508fbfdb884
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-22 20:47:05 -0500

    remove old test logic from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,9 @@ var gulp = require('gulp');
 var istanbul = require('gulp-istanbul');
 var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
-var git = require('gulp-git');
-var del = require('rimraf');
-
-var lint = ['index.js', 'lib/*.js'];
-
-function url(repo) {
-  return 'https://github.com/' + repo;
-}
 
 gulp.task('coverage', function() {
-  return gulp.src(lint)
+  return gulp.src(['index.js', 'lib/*.js'])
     .pipe(istanbul())
     .pipe(istanbul.hookRequire());
 });
@@ -26,25 +18,8 @@ gulp.task('mocha', ['coverage'], function() {
 });
 
 gulp.task('eslint', function() {
-  return gulp.src(lint)
+  return gulp.src(['*.js', 'lib/*.js', 'test/*.js'])
     .pipe(eslint())
-});
-
-gulp.task('spec', ['clone'], function(cb) {
-  gulp.src('test/_spec/test/*.js')
-    .pipe(mocha({reporter: 'spec'}))
-    .on('end', function() {
-      del('test/_spec', cb);
-    });
-});
-
-gulp.task('clone', function(cb) {
-  del('test/_spec', function(err) {
-    if (err) return cb(err);
-    git.clone(url('jonschlinkert/templates'), {
-      args: 'test/_spec'
-    }, cb);
-  });
 });
 
 gulp.task('default', ['mocha', 'eslint']);
