855bc509278c8a727ff14a0b3cd900d2ae2b14cd
Brian Woodward <brian.woodward@gmail.com>
2016-01-09 10:47:33 -0500

    formatting

--- docs/plugins/manifest.js
+++ docs/plugins/manifest.js
@@ -5,13 +5,13 @@ module.exports = function manifest (app, options) {
   var files = [];
   var rename = renamePath(options);
   return through.obj(function (file, enc, cb) {
-      files.push(rename(file.relative));
-      cb(null, file);
-    }, function (cb) {
-      var file = app.view({path: 'manifest.json', content: JSON.stringify(files, null, 2) });
-      this.push(file);
-      cb();
-    });
+    files.push(rename(file.relative));
+    cb(null, file);
+  }, function (cb) {
+    var file = app.view({path: 'manifest.json', content: JSON.stringify(files, null, 2) });
+    this.push(file);
+    cb();
+  });
 };
 
 function renamePath(options) {
