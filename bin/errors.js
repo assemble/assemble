33463f9c63cdcd2b6a3a86ed1af88c70392854d7
Brian Woodward <brian.woodward@gmail.com>
2016-01-12 13:31:32 -0500

    show error message if assemblefile does not export an instance or function

--- bin/errors.js
+++ bin/errors.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-  assemblefile: 'Cannot find assemblefile.js in the current working directory. Use `--cwd` to specify a working directory.'
+  assemblefile: 'Cannot find assemblefile.js in the current working directory. Use `--cwd` to specify a working directory.',
+  exports: 'a function or instance must be exported from assemblefile "${assemblefile}" See "docs/assemblefile.md" for details'
 };
