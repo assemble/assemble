c36b81101bc37af72aaded61856d9ad23182a7e8
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-15 11:02:26 -0500

    adds `placeholders` for resolving paths

---
+++ docs/support/utils.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var placeholders = require('placeholders');
+var utils = require('../../lib/utils');
+
+var support = module.exports;
+
+
+support.exists = utils.exists;
+
+
+support.versionPath = function(structure, data, opts) {
+  return placeholders(opts)(structure, data);
+};
