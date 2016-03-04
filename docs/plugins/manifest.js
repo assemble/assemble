f8582f4207b56061ec8fa592303ab1775b3daf80
Brian Woodward <brian.woodward@gmail.com>
2016-03-04 15:54:47 -0500

    allow passing path into manifest plugin

--- docs/plugins/manifest.js
+++ docs/plugins/manifest.js
@@ -1,18 +1,21 @@
 'use strict';
 
+var path = require('path');
 var through = require('through2');
+var extend = require('extend-shallow');
 
 module.exports = function manifest(app, options) {
-  var rename = renamePath(options);
+  var opts = extend({}, options);
+  var rename = renamePath(opts);
   var files = [];
 
   return through.obj(function(file, enc, cb) {
     files.push(rename(file.relative));
     cb(null, file);
   }, function(cb) {
-
+    var fp = opts.base ? path.join(opts.base, 'manifest.json') : 'manifest.json';
     var content = JSON.stringify(files, null, 2);
-    var file = app.view({path: 'manifest.json', content: content});
+    var file = app.view({path: fp, content: content});
     this.push(file);
     cb();
   });
@@ -20,7 +23,7 @@ module.exports = function manifest(app, options) {
 
 
 function renamePath(options) {
-  if (options && typeof options.renamePath === 'function') {
+  if (typeof options.renamePath === 'function') {
     return options.renamePath;
   }
   return function(fp) {
