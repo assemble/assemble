3115c2a2837e37b7cfa8baf147fc1e6448735749
Brian Woodward <brian.woodward@gmail.com>
2016-05-05 15:32:52 -0400

    remove unused utils and update deps

--- docs/generators/lib/utils.js
+++ docs/generators/lib/utils.js
@@ -8,12 +8,8 @@ require = utils;
  * Lazily required module dependencies
  */
 
-require('base-fs-conflicts', 'conflicts');
 require('extend-shallow', 'extend');
-require('kind-of', 'typeOf');
 require('match-file', 'match');
-require('minimist');
-require('mixin-deep', 'merge');
 require('through2', 'through');
 require = fn;
 
